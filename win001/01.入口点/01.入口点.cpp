33595370d53d576e682cf00fcb04d239b59aa257
Allenboy <32590722+haidragon@users.noreply.github.com>
2018-03-19 00:40:05 -0500

    Add files via upload

---
+++ win001/01.入口点/01.入口点.cpp
@@ -0,0 +1,19 @@
+// 01.入口点.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <windows.h>
+#include <iostream>
+using namespace std;
+// 要release模式才能运行
+// 链接器->高级->入口点
+void My24() {
+	cin.get();
+	MessageBox(0, 0, 0, 0);
+}
+
+int main()
+{
+    return 0;
+}
+
